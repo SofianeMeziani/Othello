2736e6cedee52269bb03d27196cc7c8269a0c513
Sofiane Meziani <sofiane.meziani@univ-rouen.fr>
2020-02-22 19:26:58 +0100

    parse_diag_top_bottom_right_left

--- js/script.js
+++ js/script.js
@@ -309,7 +309,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 if (tile_type(parse_row, col) == 0) {
                     
                     if (seq_begin_white != -1 && seq_end_white != -1 && seq_begin_black != -1 && seq_end_black != -1) {
-                        console.log(parse_row + ' ' + col);
+                        //console.log(parse_row + ' ' + col);
                         if ( ($white_turn && (seq_end_white <  seq_end_black)) || ($black_turn && (seq_end_black <  seq_end_white)) ) {
                             set_playable (parse_row, col);
                         }
@@ -478,7 +478,6 @@ document.addEventListener("DOMContentLoaded", function(){
             parse_row = row; 
 
             while (parse_row >= 0) {
-                console.log(row + ' , ' + col);
 
                 // white
                 if (tile_type(row, col) == 1) {
@@ -529,9 +528,144 @@ document.addEventListener("DOMContentLoaded", function(){
                 }
 
             } 
-            console.log('---'); 
+            //console.log('---'); 
             row--;
         }
+
+    }
+
+    function parse_diag_top_bottom_right_left () {
+
+        row = 7;
+        col = 0;
+
+        init_parsing_variables();
+
+
+        /* (0 , 0)
+        (0, 1) (1, 0)
+        (0, 2) (1, 1) (2, 0) */
+
+        while (row >= 0) {
+            col = 0;
+            parse_row = row;
+            while (parse_row < 8) {
+                //console.log( col + ' ' + (7 - parse_row));
+                // white
+                if (tile_type(col, (7 - parse_row)) == 1) {
+                    
+                    if (seq_begin_white == -1) {
+                        seq_begin_white = parse_row;
+                        seq_end_white = parse_row;
+                    } else {
+                        seq_end_white = parse_row;
+                    }
+                    parse_row++;
+                    col++;
+                }
+
+                // black
+                if (tile_type(col, (7 - parse_row)) == 2) {
+                    
+                    if (seq_begin_black == -1) {
+                        seq_begin_black = parse_row;
+                        seq_end_black = parse_row;
+                    } else {
+                        seq_end_black = parse_row;
+                    }
+                    parse_row++;
+                    col++;
+                }
+
+                // playable
+                if (tile_type(col, (7 - parse_row)) == 3) {
+                    parse_row++;
+                    col++;
+                    init_parsing_variables();
+                }
+
+                // empty
+                if (tile_type(col, (7 - parse_row)) == 0) {
+                    
+                    if (seq_begin_white != -1 && seq_end_white != -1 && seq_begin_black != -1 && seq_end_black != -1) {
+                        //console.log(col + ' ' + (7 - parse_row));
+                        if ( ($white_turn && (seq_end_white <  seq_end_black)) || ($black_turn && (seq_end_black <  seq_end_white)) ) {
+                            set_playable (col, (7 - parse_row));
+                        }
+                    }
+                    parse_row++;
+                    col++;
+                    init_parsing_variables();
+                }
+            } 
+            //console.log('---'); 
+            row--;
+        }
+
+        row = 7;
+        col = 0;
+
+        /*
+        (0, 5) (1, 6) (2, 7)
+        (0, 6) (1, 7)
+        (0, 7) */
+
+        while (col < 8) {
+            row = 0;
+            parse_col = col;
+            while (parse_col < 8) {
+                
+                // white
+                if (tile_type(parse_col, (7 - row)) == 1) {
+                    
+                    if (seq_begin_white == -1) {
+                        seq_begin_white = row;
+                        seq_end_white = row;
+                    } else {
+                        seq_end_white = row;
+                    }
+                    parse_col++;
+                    row++;
+                }
+
+                // black
+                if (tile_type(parse_col, (7 - row)) == 2) {
+                    
+                    if (seq_begin_black == -1) {
+                        seq_begin_black = row;
+                        seq_end_black = row;
+                    } else {
+                        seq_end_black = row;
+                    }
+                    parse_col++;
+                    row++;
+                }
+
+                // playable
+                if (tile_type(parse_col, (7 - row)) == 3) {
+                    parse_col++;
+                    row++;
+                    init_parsing_variables();
+                }
+
+                // empty
+                if (tile_type(parse_col, (7 - row)) == 0) {
+                    
+                    if (seq_begin_white != -1 && seq_end_white != -1 && seq_begin_black != -1 && seq_end_black != -1) {
+                        
+                        if ( ($white_turn && (seq_end_white <  seq_end_black)) || ($black_turn && (seq_end_black <  seq_end_white)) ) {
+                            set_playable (parse_col, (7 - row));
+                        }
+                    }
+                    parse_col++;
+                    row++;
+                    init_parsing_variables();
+                }  
+                
+            }  
+            //console.log('---*');
+            col++;
+        } 
 
     }
 
@@ -539,23 +673,19 @@ document.addEventListener("DOMContentLoaded", function(){
         clear_playable_tiles();
         // parser dans toutes les directions
        
-        // parse de haut en bas 
         parse_top_bottom();
-        // parse bas haut
         parse_bottom_top();
-        // parse gauche droite
         parse_left_right();
-        // parse droite gauche
         parse_right_left();
-
-        // parse diag haut bas gauche droite
         parse_diag_top_bottom_left_right();
+        parse_diag_bottom_top_right_left();
+
         // parse diag haut bas droite gauche
+        parse_diag_top_bottom_right_left();
         
         // parse diag bas haut gauche droite
        
-        // parse diag bas haut droite gauche
-        parse_diag_bottom_top_right_left();
+        
     }
 
     function flip (f_row, f_col) {
