410ba46f999d0d44031b6a7ffe3abcefa95afae8
Ramzi <bouchairramzi@gmail.com>
2020-06-03 23:18:31 +0100

    minimax pbm lors la recurssion

---
+++ js/evaluation.js
@@ -0,0 +1,165 @@
+/*
+ * canmove(), isLegalMove() and num_valid_moves() are helper functions to 
+ * count the number of valid moves for a given player in a given
+ * board configuration
+ */
+function isLegalMove(i, j) {
+    if (tile_type(i, j) === 3) //|| tile_type(i,j)===0)
+        return true;
+    return false;
+}
+
+function num_valid_moves() {
+    let count = 0;
+    let i, j;
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < 8; j++)
+            if (isLegalMove(i, j)) count++;
+    return count;
+}
+
+/*
+ * Assuming my_color stores your color and opp_color stores opponent's color
+ * '-' indicates an empty square on the board
+ * 'b' indicates a black tile and 'w' indicates a white tile on the board
+ */
+function dynamic_heuristic_evaluation_function(grid) {
+    let my_tiles = 0;
+    let opp_tiles = 0;
+    let my_front_tiles = 0;
+    let opp_front_tiles = 0;
+    let x, y;
+    p = 0, c = 0, l = 0, m = 0, f = 0, d = 0;
+    let V=[];
+
+    X1 = [-1, -1, 0, 1, 1, 1, 0, -1];
+    Y1 = [0, 1, 1, 1, 0, -1, -1, -1];
+
+    V[0] = [20, -3, 11, 8, 8, 11, -3, 20];
+    V[1] = [-3, -7, -4, 1, 1, -4, -7, -3];
+    V[2] = [11, -4, 2, 2, 2, 2, -4, 11];
+    V[3] = [8, 1, 2, -3, -3, 2, 1, 8];
+    V[4] = [8, 1, 2, -3, -3, 2, 1, 8];
+    V[5] = [11, -4, 2, 2, 2, 2, -4, 11];
+    V[6] = [-3, -7, -4, 1, 1, -4, -7, -3];
+    V[7] = [20, -3, 11, 8, 8, 11, -3, 20];
+
+    // Piece difference, frontier disks and disk squares
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < 8; j++) {
+            if (grid[i][j] == my_color) {
+                d += V[i][j];
+                my_tiles++;
+            } else if (grid[i][j] == opp_color) {
+                d -= V[i][j];
+                opp_tiles++;
+            }
+            if (grid[i][j] != '-') {
+                for (k = 0; k < 8; k++) {
+                    x = i + X1[k];
+                    y = j + Y1[k];
+                    if (x >= 0 && x < 8 && y >= 0 && y < 8 && grid[x][y] == '-') {
+                        if (grid[i][j] == my_color) my_front_tiles++;
+                        else opp_front_tiles++;
+                        break;
+                    }
+                }
+            }
+        }
+    if (my_tiles > opp_tiles)
+        p = (100.0 * my_tiles) / (my_tiles + opp_tiles);
+    else if (my_tiles < opp_tiles)
+        p = -(100.0 * opp_tiles) / (my_tiles + opp_tiles);
+    else p = 0;
+
+    if (my_front_tiles > opp_front_tiles)
+        f = -(100.0 * my_front_tiles) / (my_front_tiles + opp_front_tiles);
+    else if (my_front_tiles < opp_front_tiles)
+        f = (100.0 * opp_front_tiles) / (my_front_tiles + opp_front_tiles);
+    else f = 0;
+
+    // Corner occupancy
+    my_tiles = opp_tiles = 0;
+    if (grid[0][0] == my_color) my_tiles++;
+    else if (grid[0][0] == opp_color) opp_tiles++;
+    if (grid[0][7] == my_color) my_tiles++;
+    else if (grid[0][7] == opp_color) opp_tiles++;
+    if (grid[7][0] == my_color) my_tiles++;
+    else if (grid[7][0] == opp_color) opp_tiles++;
+    if (grid[7][7] == my_color) my_tiles++;
+    else if (grid[7][7] == opp_color) opp_tiles++;
+    c = 25 * (my_tiles - opp_tiles);
+
+    // Corner closeness
+    my_tiles = opp_tiles = 0;
+    if (grid[0][0] == '-') {
+        if (grid[0][1] == my_color) my_tiles++;
+        else if (grid[0][1] == opp_color) opp_tiles++;
+        if (grid[1][1] == my_color) my_tiles++;
+        else if (grid[1][1] == opp_color) opp_tiles++;
+        if (grid[1][0] == my_color) my_tiles++;
+        else if (grid[1][0] == opp_color) opp_tiles++;
+    }
+    if (grid[0][7] == '-') {
+        if (grid[0][6] == my_color) my_tiles++;
+        else if (grid[0][6] == opp_color) opp_tiles++;
+        if (grid[1][6] == my_color) my_tiles++;
+        else if (grid[1][6] == opp_color) opp_tiles++;
+        if (grid[1][7] == my_color) my_tiles++;
+        else if (grid[1][7] == opp_color) opp_tiles++;
+    }
+    if (grid[7][0] == '-') {
+        if (grid[7][1] == my_color) my_tiles++;
+        else if (grid[7][1] == opp_color) opp_tiles++;
+        if (grid[6][1] == my_color) my_tiles++;
+        else if (grid[6][1] == opp_color) opp_tiles++;
+        if (grid[6][0] == my_color) my_tiles++;
+        else if (grid[6][0] == opp_color) opp_tiles++;
+    }
+    if (grid[7][7] == '-') {
+        if (grid[6][7] == my_color) my_tiles++;
+        else if (grid[6][7] == opp_color) opp_tiles++;
+        if (grid[6][6] == my_color) my_tiles++;
+        else if (grid[6][6] == opp_color) opp_tiles++;
+        if (grid[7][6] == my_color) my_tiles++;
+        else if (grid[7][6] == opp_color) opp_tiles++;
+    }
+    l = -12.5 * (my_tiles - opp_tiles);
+
+    // Mobility
+    my_tiles = num_valid_moves(my_color, opp_color, grid);
+    opp_tiles = num_valid_moves(opp_color, my_color, grid);
+    if (my_tiles > opp_tiles)
+        m = (100.0 * my_tiles) / (my_tiles + opp_tiles);
+    else if (my_tiles < opp_tiles)
+        m = -(100.0 * opp_tiles) / (my_tiles + opp_tiles);
+    else m = 0;
+
+    // final weighted score
+    score = (10 * p) + (801.724 * c) + (382.026 * l) + (78.922 * m) + (74.396 * f) + (10 * d);
+    return score;
+}
+
+
+function count_tiles(player) {
+    let count = 0;
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if (tile_type(i, j) === player) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// fonction evaluation basique : 
+
+function evaluation_simple() {
+    my_tiles = count_tiles(1);
+    opp_tiles = count_tiles(2);
+    if (my_tiles + opp_tiles != 0)
+        return 100 * (my_tiles - opp_tiles) / (my_tiles + opp_tiles);
+    else 
+        return 0;
+}
--- js/minimax.js
+++ js/minimax.js
@@ -1,36 +1,45 @@
 var count = 0;
 
 function minimax(level) {
-    
-    minimax_timeout = setTimeout(function () {
+
+    /*minimax_timeout = setTimeout(function () {
         jQuery('.playable').random().click();
+    }, 1000);*/
+    // ici au lieu d'utiliser random utiliser bestMove
+    minimax_timeout = setTimeout(function () {
+        let mo = bestMove(level);
+        console.error("Mouvement à faire est : ", mo); 
+        play(mo);
     }, 1000);
-    // ici au lieu d'utiliser random utiliser 
 }
 
 // Retourner l'état de terrain de jeu  :
 function getBoardState() {
-    let terrain;
+    let terrain = [];
     for (i = 0; i < 8; i++) {
         let row;
         for (j = 0; j < 8; j++) {
             row = []; // vider la ligne 
-            switch (tile_type(i,j)){
-            case 0 : row[j] = 0; // vide
-            case 1 : row[j] = 1; // white 
-            case 2 : row[j] = 2; // black
-            case 3 : row[j] = 3; // playable 
+            switch (tile_type(i, j)) {
+                case 0:
+                    row[j] = 0; // vide
+                case 1:
+                    row[j] = 1; // white 
+                case 2:
+                    row[j] = 2; // black
+                case 3:
+                    row[j] = 3; // playable 
             }
-            
+
         }
-        terrain.push(row); 
+        terrain.push(row);
     }
     return terrain;
 }
 
 // retourner les mouvements possibles : 
 function getPlayableCells() {
-    let playable;
+    let playable = [];
     for (i = 0; i < 8; i++) {
         for (j = 0; j < 8; j++) {
             // si la cellule est playable alors l'ajouter à la liste de ce niveau 
@@ -42,12 +51,14 @@ function getPlayableCells() {
     return playable;
 }
 
+// cette fonction pour évaluer un mouvement.
+function evaluate(board) {
+    
 
-// est ce que on évalue à partir de la liste des cases qu'on peut joué 
-// ou bien à partir de tout le terrain 
-function evaluate(playables) {
-    // cette fonction pour évaluer un mouvement.
-
+    let eval = evaluation_simple(1, 2);
+    //let eval = dynamic_heuristic_evaluation_function(board);
+    console.log('evaluation ', eval);    
+        return eval;
 }
 
 // c'est cette fonction qui va retrouner le mouvement à faire au lieu de random 
@@ -55,20 +66,26 @@ function bestMove(depth) {
     let bestScore = Number.NEGATIVE_INFINITY;
     let move;
     let playables = getPlayableCells();
-    playables.forEach(mouvement => {
+    console.log("playables : ", playables);
+    for (i = 0; i < playables.length; i++) {
+        //for(mouvement in playables) {
         // pour chaque mouvement possible on joue et on appel minimax
-        play(mouvement)
+        mouvement=playables[i];
+        console.log("Mouvement : ", mouvement);
+        play(mouvement);
         // appeler minimax recursivement : en passant les cells playables
-        let plays = getPlayableCells();
-        let score = minimax(plays, depth - 1, is_max_player);
+        let board = getBoardState();
+        console.warn("depth bestMove: ", depth);
+        let score = minimax2(board, depth - 1, $white_turn);
+        console.log("Le score est :", score)
         // unplay pour revenir à l'état précédent 
         unplay();
         // bestScore = Math.max(score, bestScore); 
-        if (score > bestScore) {
+        if (score >= bestScore) {
             bestScore = score;
             move = mouvement;
         }
-    });
+    };
     // à la fin de traitment on joue le meilleur coup possible 
     // retourner le move et on continue le traitement dans la fonction principale 
     return move;
@@ -77,11 +94,13 @@ function bestMove(depth) {
 // est ce que je passe la liste des playables ou toutes le terrain 
 // ça dépende de la fonction d'évaluation. 
 
-function minimax2(plays, depth, is_max_player) {
+function minimax2(board, depth, is_max_player) {
     // normalement je vérifie si il y a encore des mouvement ou pas 
     // si je suis dans une feuille (leaf) ou il y a plus de mouvement possible
-    if (depth == 0 || plays!=[]) {
-        return evaluate(plays);
+    plays = getPlayableCells();
+    if (depth < 1) { 
+        console.error("rje3 evaluation te3 minimax depth<1");       
+        return evaluate(board);
         // retourner l'évaluation normalment 
     }
 
@@ -90,26 +109,30 @@ function minimax2(plays, depth, is_max_player) {
         let bestScore = Number.NEGATIVE_INFINITY;
         let playables = getPlayableCells();
 
-        playables.forEach(mouvement => {
-            play(mouvement); 
-            let plays = getPlayableCells();
-            let score = minimax(plays, depth - 1, true);
-            unplay(mouvement); 
+        for (i = 0; i < playables.length; i++) {
+            mouvement = playables[i];
+            play(mouvement);
+            let terrain = getBoardState();
+            console.warn("depth max: ", depth)
+            let score = minimax2(terrain, depth - 1, false);
+            unplay();
             bestScore = Math.max(score, bestScore);
-        });
+        };
         return bestScore;
     } else {
         // minimizing player 
         let bestScore = Number.POSITIVE_INFINITY;
         let playables = getPlayableCells();
 
-        playables.forEach(mouvement => {
+        for (i = 0; i < playables.length; i++) {
+            mouvement = playables[i];
             play(mouvement);
-            let plays = getPlayableCells(); 
-            let score = minimax(plays, depth - 1, false);
-            unplay(mouvement); 
+            let terrain = getBoardState();
+            console.warn('depth min: ', depth); 
+            let score = minimax2(terrain, depth - 1, true);
+            unplay();
             bestScore = Math.min(score, bestScore);
-        });
+        };
         return bestScore;
     }
 }
